e25cf09dd85e2d30a21543a2d4551764fd891076
Harrison Sayre <hsayre42@gmail.com>
2025-06-09 09:57:55 -0400

    Practiced C# type fundementals.

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -6,11 +6,6 @@ Example
 
 Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
 
-Console.WriteLine("Please enter your name: ");
-
-//The following will accept the name
-string name = Console.ReadLine();
-
 //string name2 = Console.ReadLine();
 
 //string name_2 = Console.ReadLine();
@@ -18,3 +13,93 @@ string name = Console.ReadLine();
 //string Name_2 = Console.ReadLine();
 
 //string 2Name = Console.ReadLine();
+
+var monthlyWage = 1234;
+
+//Two variables declared in one line
+int yearlyWage = 12, bonus = 1000;
+
+var isActive = true;
+
+var rating = 99.25;
+
+var numberOfEmployees = 300; //Now thati t's var will think it's int, not byte, and work
+
+int hoursWorked;
+
+hoursWorked = 125;
+
+hoursWorked = 148;
+
+//monthlyWage = true //Does not work because type safety.
+
+const double interestRate = 0.07;
+
+string firstName = "Bethany";
+string lastName = "Smith";
+string emptyString = "";
+
+Console.WriteLine("Please enter your name: ");
+
+//The following will accept the name
+string name = Console.ReadLine();
+
+double ratePerHour = 12.34;
+int numberOfHoursWorked = 165;
+
+double currentMonthWage = ratePerHour * numberOfHoursWorked + bonus;
+Console.WriteLine(currentMonthWage);
+
+ratePerHour += 3; //ratePerHour = ratePerHour+3
+
+if (currentMonthWage > 2000)
+    Console.WriteLine("Top Paid employee");
+
+//int numberOfEmployees = 15;
+numberOfEmployees--;
+
+bool a;
+int b;
+
+int intMaxValue = int.MaxValue;
+int minValue = int.MinValue;
+
+char userSelection = 'a';
+char upperVersion = char.ToUpper(userSelection);
+
+bool isDigit = char.IsDigit(upperVersion);
+bool isLetter = char.IsLetter(upperVersion);
+
+
+
+var hireDate = new DateTime(2022, 3, 28, 14, 30, 0);
+DateTime exitDate = new DateTime(2025, 12, 11);
+
+//DateTime invalidDate = new DateTime(2025, 15, 11);
+DateTime startDate = hireDate.AddDays(15);
+
+DateTime startHour = DateTime.Now;
+TimeSpan workTime = new TimeSpan(8, 35, 0);
+DateTime endHour = startHour.Add(workTime);
+Console.WriteLine(startHour.ToLongDateString());
+
+int numberofHoursWorked = 165;
+
+long veryLongMonth = numberofHoursWorked; //Works fine
+
+double d = 123456789.0;
+
+int x = (int)d;
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
