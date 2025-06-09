d65610e092be6dc0c850f811411e2aa3149e62c9
Harrison Sayre <hsayre42@gmail.com>
2025-06-09 13:39:51 -0400

    Practiced with Strings in #C

--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -3,12 +3,8 @@
 using BethanysPieShopHRM;
 using System.Numerics;
 
-int amount = 1234;
-int months = 12;
-int bonus = 1000;
-Utilities.CalculateYearlyWage(amount, months, bonus);
-int yearlywage = Utilities.CalculateYearlyWage(amount, months);
-
-Console.WriteLine($"Yearly wage: {yearlywage}");
+//Utilities.ManipulatingStrings();
+//Utilities.UsingEscapeCharacters();
+Utilities.ParsingStrings();
 
 Console.ReadLine();
