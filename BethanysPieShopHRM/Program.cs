59124b894482bd99771126f43fe5a11910572939
Harrison Sayre <hsayre42@gmail.com>
2025-06-09 09:08:30 -0400

    Add project files.

---
+++ BethanysPieShopHRM/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Practice for block commments
+Example
+*/
+
+Console.WriteLine("Welcome to Bethany's Pie Shop HRM");
+
+Console.WriteLine("Please enter your name: ");
+
+//The following will accept the name
+string name = Console.ReadLine();
+
+//string name2 = Console.ReadLine();
+
+//string name_2 = Console.ReadLine();
+
+//string Name_2 = Console.ReadLine();
+
+//string 2Name = Console.ReadLine();
